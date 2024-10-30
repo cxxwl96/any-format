98425205b2a14cfb1bc449422e7eb1aa1c676d8f
cxxwl96 <cxxwl96@sina.com>
2024-10-30 21:37:57 +0800

    log format

--- src/views/runner/JSRunner.vue
+++ src/views/runner/JSRunner.vue
@@ -2,6 +2,7 @@
 import { CodeMirror } from '@/components/CodeEditor'
 import { ref } from 'vue'
 import './Context'
+import { getTextFromClipboard } from '@/utils/useCopyToClipboard'
 
 const script = ref<string>()
 const result = ref<{ data: any; success: boolean; }>({data: '', success: true})
@@ -27,11 +28,11 @@ const handleRunner = () => {
   <AButton @click="handleRunner">运行脚本</AButton>
   <ARow :gutter="16" style="height: calc(100vh - 300px)">
     <ACol :span="12">
-      <div class="tip-font">脚本：</div>
+      <div class="tip-font">脚本：<a @click="async () => {script = await getTextFromClipboard()}">粘贴脚本</a></div>
       <CodeMirror v-model="script" theme="darcula" mode="javascript" style="height: 100%" />
     </ACol>
     <ACol :span="12">
-      <div class="tip-font">脚本：</div>
+      <div class="tip-font">运行结果：</div>
       <ATextarea v-model:value="result.data" :style="{height: '100%', color: result.success ? 'black' : 'red'}" />
     </ACol>
   </ARow>
