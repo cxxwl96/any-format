9c0149a0fc6012c360581b59f3f4ad3f3a26bd92
cxxwl96 <cxxwl96@sina.com>
2024-10-31 20:51:01 +0800

    js runner

--- src/views/runner/JSRunner.vue
+++ src/views/runner/JSRunner.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
 import { CodeMirror } from '@/components/CodeEditor'
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 import './Context'
 import { getTextFromClipboard } from '@/utils/useCopyToClipboard'
 
-const script = ref<string>()
-const result = ref<{ data: any; success: boolean; }>({data: '', success: true})
+const props = defineProps({ modelValue: String })
+const emits = defineEmits(['update:modelValue'])
+const script = ref<string>(props.modelValue || '')
+const result = ref<{ data: any; success: boolean; }>({ data: '', success: true })
+watch(() => props.modelValue, (value) => script.value = value || '')
 
 const handleRunner = () => {
   if (script.value) {
@@ -21,24 +24,40 @@ const handleRunner = () => {
     result.value.success = false
   }
 }
+const handleChange = (value: string) => {
+  emits('update:modelValue', value)
+}
+// 暴露方法
+defineExpose({
+  run: () => handleRunner()
+})
 </script>
 
 <template>
-<div>
-  <AButton @click="handleRunner">运行脚本</AButton>
-  <ARow :gutter="16" style="height: calc(100vh - 300px)">
-    <ACol :span="12">
-      <div class="tip-font">脚本：<a @click="async () => {script = await getTextFromClipboard()}">粘贴脚本</a></div>
-      <CodeMirror v-model="script" theme="darcula" mode="javascript" style="height: 100%" />
-    </ACol>
-    <ACol :span="12">
-      <div class="tip-font">运行结果：</div>
-      <ATextarea v-model:value="result.data" :style="{height: '100%', color: result.success ? 'black' : 'red'}" />
-    </ACol>
-  </ARow>
-</div>
+  <div class="js-runner">
+    <AButton type="primary" @click="handleRunner" class="run-btn">运行脚本</AButton>
+    <div class="tip-font">脚本：<a @click="async () => {script = await getTextFromClipboard()}">粘贴脚本</a></div>
+    <CodeMirror v-model="script" @change="handleChange" theme="darcula" mode="javascript" />
+    <div class="tip-font">运行结果：</div>
+    <ATextarea v-model:value="result.data" :style="{color: result.success ? 'black' : 'red'}" />
+  </div>
 </template>
 
 <style scoped>
-
+.js-runner {
+  .tip-font {
+    display: inline-block;
+  }
+  .run-btn {
+    float: right;
+  }
+  :global(.CodeMirror) {
+    min-height: 300px;
+    max-height: calc(100vh - 350px);
+  }
+  :global(.CodeMirror-scroll) {
+    min-height: 300px;
+    max-height: calc(100vh - 350px);
+  }
+}
 </style>
