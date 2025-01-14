77da080f77a971a0800e345a8d333288669e4b1b
cxxwl96 <cxxwl96@sina.com>
2025-01-14 22:51:11 +0800

    diff

--- src/views/comparator/TextComparator.vue
+++ src/views/comparator/TextComparator.vue
@@ -7,19 +7,19 @@ import cssWorker from "monaco-editor/esm/vs/language/css/css.worker?worker";
 import htmlWorker from "monaco-editor/esm/vs/language/html/html.worker?worker";
 import tsWorker from "monaco-editor/esm/vs/language/typescript/ts.worker?worker";
 
-import { onBeforeMount, onBeforeUnmount, onMounted, ref } from 'vue'
-
+import { onBeforeUnmount, onMounted, ref, watch } from 'vue'
 
 const editorRef = ref()
 let editor: monaco.editor.IStandaloneDiffEditor;
 let originModel: monaco.editor.ITextModel
 let modifiedModel: monaco.editor.ITextModel
 
+// 初始化编辑器
 onMounted(() => initDiffEditor())
-onBeforeUnmount(()=> {
-  editor && editor.dispose()
-})
+// 销毁编辑器
+onBeforeUnmount(()=> editor && editor.dispose())
 
+// 初始化环境
 self.MonacoEnvironment = {
   getWorker(_, label) {
     if (label === "json") {
@@ -37,7 +37,17 @@ self.MonacoEnvironment = {
     return new editorWorker();
   },
 };
-
+const hideUnchangedRegions = ref<boolean>(false)
+watch(()=>hideUnchangedRegions.value, (value: boolean) => {
+  editor.updateOptions({
+    hideUnchangedRegions: {
+      enabled: value,
+      revealLineCount: 1,
+      minimumLineCount: 1,
+      contextLineCount: 1,
+    }
+  })
+})
 const initDiffEditor = () => {
   editor = monaco.editor.createDiffEditor(editorRef.value, {
     theme: 'vs', // 主题，'vs','vs-dark','hc-black','hc-light'
@@ -46,30 +56,45 @@ const initDiffEditor = () => {
     originalEditable: true, // 是否允许修改原始文本
     automaticLayout: true, // 控制编辑器是否自动调整布局以适应容器大小的变化
     renderSideBySide: true, // 是否side模式
-    enableSplitViewResizing: true,
+    enableSplitViewResizing: true, // 允许调整“差异编辑器”拆分视图的大小Defaults to true.
     roundedSelection: true, // 圆角文本选择
     ignoreTrimWhitespace: false, // 忽略空白
     diffWordWrap: 'off', // 自动换行
     wordWrap: 'off', // 自动换行
     fontSize: 14, // 字体大小
+    renderIndicators: false, // 为添加/删除的更改呈现+/-指示器.Defaults to true.
+    hideUnchangedRegions: {
+      enabled: hideUnchangedRegions.value,
+      revealLineCount: 1,
+      minimumLineCount: 1,
+      contextLineCount: 1,
+    },
+    onlyShowAccessibleDiffViewer: false, //只显示差异审查模式。
+
     dropIntoEditor: {
       enabled: true // 能否把文字拖拽进编辑器
     },
     minimap: {
-      enabled: false, // 是否启用代码缩略图
+      enabled: true, // 是否启用代码缩略图
     },
     cursorBlinking: 'phase', // 光标动画样式
     overviewRulerBorder: false, // 滚动是否有边框
     cursorSmoothCaretAnimation: 'explicit', // 控制光标平滑动画的开启与关闭。当开启时，光标移动会有平滑的动画效果。
     mouseWheelZoom: true, //设置是否开启鼠标滚轮缩放功能
-    folding: true, //控制是否开启代码折叠功能
+    folding: true, // 是否折叠
+    foldingHighlight: true, // 折叠等高线
+    foldingStrategy: 'auto', // 折叠方式
+    showFoldingControls: 'always', // 是否一直显示折叠
     diffCodeLens: true,
     scrollbar: {
       verticalScrollbarSize: 5, // 垂直滚动条宽度，默认px
-      horizontalScrollbarSize: 2, // 水平滚动条高度
+      horizontalScrollbarSize: 5, // 水平滚动条高度
     },
     scrollBeyondLastLine: false, // 启用该功能，滚动可以在最后一行之后增加一个屏幕大小，默认值为 true
-    renderLineHighlight: 'line', // 启用当前行高亮显示的呈现
+    stickyScroll: {
+      enabled: false, // 取消顶部吸附
+    },
+    renderLineHighlight: 'all', // 启用当前行高亮显示的呈现
     contextmenu: false, // 右键菜单
     columnSelection: false, // 启用列编辑 按下shift键位然后按↑↓键位可以实现列选择 然后实现列编辑
     copyWithSyntaxHighlighting: true, // 是否应将语法突出显示复制到剪贴板中 即 当你复制到word中是否保持文字高亮颜色
@@ -81,9 +106,6 @@ const initDiffEditor = () => {
     glyphMargin: false, // 设置是否显示装饰符号边距
     unicodeHighlight: {
       ambiguousCharacters: false, // 禁用 unicode 字符的高亮显示
-    },
-    stickyScroll: {
-      enabled: false, // 取消顶部吸附
     },
   })
 
@@ -116,15 +138,13 @@ const initDiffEditor = () => {
     modified: modifiedModel,
   })
 }
-
 </script>
 
 <template>
-<div>
-  <div ref="editorRef" style="height: 1000px"></div>
-</div>
+  <a-switch v-model:checked="hideUnchangedRegions" />
+  <div ref="editorRef" v-bind="$attrs" style="height: calc(100vh - 154px)"/>
 </template>
 
 <style scoped>
-
+.hidden-line { display: none !important; }
 </style>
